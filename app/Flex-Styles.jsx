5ecdf512f9490c4f038a34a854d258d47d9a5981
Rohit kumar <rohitprj.1282@gmail.com>
2024-06-15 15:41:24 +0530

    r

--- app/Flex-Styles.jsx
+++ app/Flex-Styles.jsx
@@ -1,86 +1,91 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={{
-            height: 500,
-            width: "100%",
-            backgroundColor: "skyblue",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "center", // flex-start | center | flex-end 
-            // space-evenly | space-between | space-around 
-
-            // to center horizontally-----------------------------------------------------------------
-            alignContent: "center"   // flex-start | center | flex-end | stretch
-            // space-evenly | space-between | space-around 
-
-
-        }}>
+        <ScrollView>
             <View style={{
-                height: 80,
-                width: 80,
-                alignSelf: "center",
-                backgroundColor: "silver",
-                // flexGrow: 2     // flex property
+                height: 500,
+                width: "100%",
+                backgroundColor: "skyblue",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "center", // flex-start | center | flex-end 
+                // space-evenly | space-between | space-around 
+
+                // to center horizontally-----------------------------------------------------------------
+                alignContent: "center"   // flex-start | center | flex-end | stretch
+                // space-evenly | space-between | space-around 
+
+
             }}>
-                <Text style={{justifyContent:"center"}}>
-                    1
-                </Text>
+                <View style={{
+                    height: 80,
+                    width: 80,
+                    alignSelf: "center",
+                    backgroundColor: "silver",
+                    // flexGrow: 2     // flex property
+                }}>
+                    <Text style={{ justifyContent: "center" }}>
+                        1
+                    </Text>
+
+                </View>
+                <View style={{
+                    height: 80,
+                    width: 80,
+                    backgroundColor: "red",
+                    // flexGrow: 1       // flex property
+                }}>
+                    <Text>
+                        2
+                    </Text>
+
+                </View>
+                <View style={{
+                    height: 80,
+                    width: 80,
+                    backgroundColor: "green"
+                }}>
+                    <Text>
+                        3
+                    </Text>
+
+                </View>
+                <View style={{
+                    height: 80,
+                    width: 80,
+                    backgroundColor: "pink"
+                }}>
+                    <Text>
+                        4
+                    </Text>
+
+                </View>
+                <View style={{
+                    height: 80,
+                    width: 80,
+                    backgroundColor: "grey"
+                }}>
+                    <Text>
+                        5
+                    </Text>
+
+                </View>
+                <View style={{
+                    height: 80,
+                    width: 80,
+                    backgroundColor: "orange"
+                }}>
+                    <Text>
+                        6
+                    </Text>
+
+                </View>
 
             </View>
-            <View style={{
-                height: 80,
-                width: 80,
-                backgroundColor: "red",
-                // flexGrow: 1       // flex property
-            }}>
-                <Text>
-                    2
-                </Text>
-
-            </View>
-            <View style={{
-                height: 80,
-                width: 80,
-                backgroundColor: "green"
-            }}>
-                <Text>
-                    3
-                </Text>
-
-            </View>
-            <View style={{
-                height: 80,
-                width: 80,
-                backgroundColor: "pink"
-            }}>
-                <Text>
-                    4
-                </Text>
-
-            </View>
-            <View style={{
-                height: 80,
-                width: 80,
-                backgroundColor: "grey"
-            }}>
-                <Text>
-                    5
-                </Text>
-
-            </View>
-            <View style={{
-                height: 80,
-                width: 80,
-                backgroundColor: "orange"
-            }}>
-                <Text>
-                    6
-                </Text>
-
-            </View>
-
-        </View>
+            <Text>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ut, consequatur similique nostrum culpa minus laborum animi cum perspiciatis consequuntur nesciunt dignissimos est assumenda consectetur praesentium quibusdam maxime iusto dolorem eos aperiam harum labore debitis vero. Fugiat neque rem eligendi dicta ut fuga, modi sequi similique nostrum commodi facere totam omnis suscipit quo provident ratione repudiandae iure sapiente porro consequatur! Placeat possimus id molestias architecto deleniti! Necessitatibus commodi eaque incidunt fugiat explicabo voluptatum quo iusto corporis possimus ad? Ullam enim possimus similique quis blanditiis unde fugit quasi nostrum voluptates esse delectus, totam porro aspernatur et. Dolorem amet temporibus tenetur expedita fugit magnam nobis cupiditate suscipit, itaque veritatis, esse tempore sequi. Commodi porro corporis unde eius nisi illum consequuntur, dolor eum voluptatem nesciunt deserunt consequatur deleniti dolores maiores magni sapiente, quia, earum ipsum minus explicabo! Atque voluptatum nostrum tempora ducimus mollitia. Dicta quas aliquid eius aliquam unde, ut quibusdam inventore accusantium reprehenderit aperiam optio, maiores earum vitae sed ab amet voluptates nihil dolorem minima similique exercitationem id ipsum? Dignissimos alias hic quam, dolore tenetur, rerum incidunt illum enim ea, cum aut unde minima beatae ipsa accusamus. Doloribus voluptatem cum est tempore? Et odio nostrum cupiditate provident. Obcaecati, officiis blanditiis accusamus maxime commodi voluptatum, nisi eaque quos quo eligendi non, quisquam doloremque incidunt? Reprehenderit assumenda accusamus nam accusantium at velit sapiente recusandae odio quisquam similique laudantium ipsa explicabo officia nesciunt aliquid nisi neque facilis voluptas ab placeat corrupti a ipsam, molestias ipsum? Assumenda tempora porro dolore fuga fugiat est quibusdam sequi? Excepturi porro dolorum, eaque hic minima quae, facilis expedita sunt quo ipsa iusto. Minus voluptatem adipisci quia, tempore harum quis molestias earum, iste saepe dolores soluta similique eius provident in temporibus ut quibusdam illo ipsam optio error, quos placeat fugit. Voluptatibus delectus quisquam, quod totam excepturi ipsam, blanditiis cum aliquam consectetur beatae debitis facilis tempora ratione nihil? Minus aliquam repellat quos quae dolorem, delectus eligendi. Deleniti nesciunt maxime ab dolorum totam, voluptas ducimus harum consequuntur consequatur officia at ratione odit porro doloremque nobis quis numquam quam eaque id! Reprehenderit iure consequuntur sint ex molestiae non tenetur nemo nihil, et eum culpa iste est veniam fugit harum quo aliquid quod pariatur voluptas a. Earum deleniti ipsum saepe delectus soluta, explicabo error aspernatur pariatur similique aperiam neque dolore, rem odit sed a cum dignissimos consequuntur quo asperiores autem ut tempore molestias. Numquam, neque tenetur earum odit explicabo deleniti nulla unde perferendis. Commodi est similique nostrum tempore rem, recusandae tempora adipisci iste itaque pariatur excepturi iure accusantium consequuntur in beatae quae aspernatur odio sed! Ullam tempore maxime earum nihil ex esse eaque ducimus, perferendis quibusdam aliquam consectetur mollitia et temporibus! Labore vero provident eius blanditiis! Quisquam blanditiis cupiditate itaque assumenda! Eaque doloribus, porro corporis necessitatibus facilis vel sapiente molestias rem. Eos praesentium deserunt aliquid, non, maiores numquam magni, obcaecati rerum aut nobis dolorem! Impedit perferendis mollitia voluptate adipisci dolore distinctio modi totam est blanditiis consequuntur suscipit quibusdam minima sed quidem voluptas, officiis soluta cumque. Possimus quos quo ipsum ipsam nostrum odit alias nesciunt consequatur, reprehenderit ducimus culpa qui eos. Rem aliquid harum quasi? Veniam qui eius quibusdam, omnis, porro vitae, nihil officiis rem aut corporis molestias dolorem doloremque. Vel minima quod sunt ipsam numquam alias voluptatum. Maxime sint voluptatum sit repellat error perspiciatis soluta tempora quasi quaerat quisquam atque quia unde aspernatur maiores accusamus cupiditate aut est illum ad illo, culpa cumque dolores distinctio. Aliquam iste voluptate, aperiam odio id, reiciendis illum in doloribus quia eius quidem tempore quae reprehenderit! Hic natus blanditiis ad error reiciendis autem cumque, recusandae iusto officiis, aspernatur quidem tempore itaque suscipit distinctio totam? Officia eaque delectus dolorum molestiae excepturi ipsum cum facilis dolorem voluptas maiores, porro esse possimus nisi illo quam ex sequi perspiciatis laudantium beatae sit in eum? Quo suscipit quas sed nam dicta! Nisi itaque ullam magni excepturi unde, fuga recusandae debitis sapiente? Laborum corrupti quisquam inventore maxime porro, aperiam eveniet nulla, odio minima sunt nisi veniam non minus atque repellendus facilis. Eaque adipisci sed cupiditate odit vero voluptatibus animi vitae fuga dolores voluptatem officiis voluptate reiciendis, sequi architecto aspernatur perferendis quibusdam, minus laboriosam pariatur accusantium inventore quos nemo. Nesciunt mollitia, quas accusantium magni nobis quaerat ipsum doloribus ducimus vitae distinctio. Rerum hic delectus quae tempore! Officiis blanditiis itaque nemo reprehenderit amet laudantium. Atque quasi asperiores quia, aperiam cupiditate deleniti, eveniet quaerat ut voluptatem eligendi, tempore maiores. Excepturi soluta perspiciatis nisi dicta dolorum. Molestias atque natus aspernatur repudiandae? Earum dolorem doloremque nobis cum laborum, enim optio natus. Suscipit quo in eos, impedit nesciunt, sint optio itaque facilis aspernatur quam possimus molestias nemo. Ad ratione perferendis optio rem nemo nobis omnis, dolorum molestias beatae aperiam eaque dicta tenetur iusto sed culpa est alias fugit non quod consectetur quo? Cum laudantium dolore commodi magnam incidunt facere repudiandae unde nemo suscipit amet rerum ea quibusdam asperiores sequi blanditiis numquam exercitationem distinctio, quas debitis iste ducimus doloribus maxime aliquam! Repellendus, consequuntur. Impedit quos est error esse facere, suscipit quia quas sint recusandae voluptate velit. Doloremque neque sunt facere ullam fugit voluptatum eius officia! Quam perspiciatis velit consequatur laudantium fugiat, numquam fuga, rem non vitae aliquid deserunt sunt repudiandae nobis exercitationem dicta nostrum quas a quaerat. Consequuntur et autem porro minus accusamus laboriosam impedit vitae esse? Non placeat, vero, numquam repellat excepturi cum facilis quo dolor sunt similique quam voluptates fugiat totam? Temporibus provident ipsum facere eius architecto, excepturi harum eum beatae, porro magnam soluta cum incidunt, dolor nobis quos impedit vel dignissimos. Omnis iste ad perferendis qui autem, mollitia minima alias iure, odit quia aperiam doloremque, accusantium dolores. Consequatur dolorum inventore maiores eveniet dignissimos voluptatibus beatae natus unde laborum, perferendis commodi sequi, dolores possimus reiciendis distinctio totam. Natus, accusantium aliquam nostrum accusamus blanditiis inventore similique veritatis provident iste distinctio ea ratione hic tenetur et nesciunt voluptatibus velit harum porro reprehenderit! Sit voluptatibus molestiae fuga similique? Recusandae repudiandae error ut, reprehenderit nesciunt officiis illo quis voluptatem nisi voluptate amet explicabo aperiam unde, quasi fugiat obcaecati provident earum, sed ducimus laborum! Voluptate animi odit beatae! Earum aliquam distinctio in labore molestiae similique numquam.
+            </Text>
+        </ScrollView>
     )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,77 +1,97 @@
-// import { View, Text, SectionList,ScrollView} from 'react-native'
-// import React from 'react'
-
-// export default function App() {
-//     const Data = [
-//         {
-//             title: 'Sports',
-//             data: ['Cricket', 'Football', 'Chess', 'Hockey']
-//         },
-//         {
-//             title: 'Players',
-//             data: ['Virat', 'Rohit', 'Hardik', 'Rinku']
-//         }
-//     ]
-//     return (
-//         <ScrollView>
-//             <SectionList
-//                 sections={Data}
-//                 keyExtractor={(item, index) => item + index}
-//                 renderItem={({ item,index }) => (
-//                     <View>
-//                         {/* <Text>{index}</Text> */}
-//                         <Text style={{color:"red",fontSize:30,borderColor:"blue",height:50,width:120,borderWidth:4,marginTop:8,marginLeft:8}}>{item}</Text>
-//                     </View>
-//                 )}
-//                 renderSectionHeader={({ section: { title } }) => (
-//                     <Text style={{color:"blue",fontSize:40,borderColor:"red",height:70,width:140,borderWidth:4}}>{title}</Text>
-//                 )}
-//             />
-//             <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
-//             <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
-//             <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
-//             <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
-//             <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
-//         </ScrollView>
-//     )
-// }
-
-import { useState } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SectionList,ScrollView} from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
 
 export default function App() {
-    const [image, setImage] = useState("")
-
-    async function fetchData() {
-        try {
-            const data = await fetch("https://dog.ceo/api/breeds/image/random")
-            const response = await data.json();
-
-            console.log(response)
-
-
-            if (response.status === "success") {
-                setImage(response.message);
-            }
-            console.log(response)
+    const Data = [
+        {
+            title: 'Sports',
+            data: ['Cricket', 'Football', 'Chess', 'Hockey']
+        },
+        {
+            title: 'Players',
+            data: ['Virat', 'Rohit', 'Hardik', 'Rinku']
         }
-        catch (error) {
-            console.log(error);
-        }
-    }
-    fetchData();
-
+    ]
     return (
-        <View style={{
-            backgroundColor: "orange",
-            width: 200,
-            height: 400,
-            marginLeft: "auto",
-            marginRight: "auto"
-        }}>
-            {Image}
-            <img src='image' />
-            <Text>Show</Text>
-        </View>
+        <ScrollView>
+            <Link href={"Flex-Styles"} style={{ borderWidth: 2, height: 30, width: 80 }}>Flex Styles</Link>
+            <SectionList
+                sections={Data}
+                keyExtractor={(item, index) => item + index}
+                renderItem={({ item,index }) => (
+                    <View>
+                        {/* <Text>{index}</Text> */}
+                        <Text style={{color:"red",fontSize:30,borderColor:"blue",height:50,width:120,borderWidth:4,marginTop:8,marginLeft:8}}>{item}</Text>
+                    </View>
+                )}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text style={{color:"blue",fontSize:40,borderColor:"red",height:70,width:140,borderWidth:4}}>{title}</Text>
+                )}
+            />
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum labore, adipisci, ex ipsum, enim ad maiores repellat nam quisquam ut a nihil cum. Sequi asperiores, consectetur tempore fugiat maiores porro sint dolor ut, autem voluptates sapiente fuga pariatur nam itaque velit modi aliquam libero odit! Ea ex veritatis modi.</Text>
+        </ScrollView>
     )
 }
+
+
+
+
+// import { useState } from 'react'
+// import { View, Text } from 'react-native'
+
+// export default function App() {
+//     const [image, setImage] = useState("")
+
+//     async function fetchData() {
+//         try {
+//             const data = await fetch("https://dog.ceo/api/breeds/image/random")
+//             const response = await data.json();
+
+//             console.log(response)
+
+
+//             if (response.status === "success") {
+//                 setImage(response.message);
+//             }
+//             console.log(response)
+//         }
+//         catch (error) {
+//             console.log(error);
+//         }
+//     }
+//     fetchData();
+
+//     return (
+//         <View style={{
+//             backgroundColor: "orange",
+//             width: "100%",
+//             height: "100%",
+//             marginLeft: "auto",
+//             marginRight: "auto"
+//         }}>
+//             {image}
+//             <img src='image' />
+//             <Text>Show</Text>
+//         </View>
+//     )
+// }
